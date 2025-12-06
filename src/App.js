884d77bbaafaf4ebe577683424159e112788c4d2
mr.mewadi <kamleshpurbiya2004@gmail.com>
2025-12-06 17:30:17 +0530

    google visit link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./templates/Login.jsx";
 import CreateAccount from "./templates/CreateAccount.jsx";
 import ForgotEmail from "./templates/ForgotEmail.jsx";
